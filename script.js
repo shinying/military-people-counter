71c5f8aa001bbd569a570e5383b58179b22abe07
shinying <shinying.lees@gmail.com>
2023-05-09 13:37:11 +0800

    add reason controlling

--- script.js
+++ script.js
@@ -1,110 +1,265 @@
 const OFFICER_BTN = "btn-outline-danger";
-const OFFICER_BTN_SELECTED = "btn-danger";
 const SERGENT_BTN = "btn-outline-info";
-const SERGERT_BTN_SELECTED = "btn-info";
 const SOILDER_BTN = "btn-outline-secondary";
-const SOILDER_BTN_SELECTED = "btn-secondary";
 const RANKS = ["#officer", "#sergeant", "#soilder"];
 const BTN_CLASS = [OFFICER_BTN, SERGENT_BTN, SOILDER_BTN];
-const BTN_SELECTED_CLASS = [OFFICER_BTN_SELECTED, SERGERT_BTN_SELECTED, SOILDER_BTN_SELECTED];
 
 
 $(function() {
-    let counts = 0;
     let member_num = [0, 0, 0];
     let member_rank = {};
-    let selected_members = new Set();
+    
+    let selected_reason;
+    let delete_mode = false;
+    
+    let reasons = [];
+    let members = [];
+
+    let members_to_delete = new Set();
+    let reasons_to_delete = new Set();
+
+    let hash = function(value) { return MD5.generate(value).substr(16); };
+
+    function activate(item, fn) {
+        item.addClass("active");
+        item.off("click");
+        item.click(fn);
+    }
+
+    function deactivate(item, fn) {
+        item.removeClass("active");
+        item.off("click");
+        item.click(fn);
+    }
+    
 
     function read_from_local() {
         if (localStorage.length == 0)
             return;
-        cache = [];
-        for (let key in localStorage) {
-            let item = localStorage.getItem(key);
-            if (!item) continue;
-            let member = JSON.parse(item);
-            cache.push(member);
-            counts = Math.max(counts, member["index"]);
-        }
-        if (!cache[0].hasOwnProperty("index") || !cache[0].hasOwnProperty("key")) {
-            console.log("Clear localStorage")
+        if (!localStorage.hasOwnProperty("reasons") && !localStorage.hasOwnProperty("members")) {
+            console.log("No reasons and members. Clear localStorage")
             localStorage.clear();
-            counts = 0;
             return;
         }
-        cache.sort(function(a, b) { return a.index - b.index; });
-        for (let member of cache) {
-            add_member(member["name"], member["rank"], member["key"]);
-            if (member["selected"]) {
-                select_member($(`#${member["key"]}`));
+
+        // Read reasons
+        if ("reasons" in localStorage)
+            reasons = JSON.parse(localStorage.getItem("reasons"));
+        for (const reason of reasons) {
+            add_reason(localStorage.getItem(`r-${reason}`, reason));
+        }
+
+        // Read members
+        if ("members" in localStorage)
+            members = JSON.parse(localStorage.getItem("members"));
+        for (const member of members) {
+            let item = localStorage.getItem(`m-${member}`);
+            if (!item) continue;
+            let detail = JSON.parse(item);
+
+            if (!detail.hasOwnProperty("key")) {
+                console.log("No key. Clear localStorage")
+                localStorage.clear();
+                return;
+            }
+
+            add_member(detail["name"], detail["rank"], detail["key"]);
+            if (detail["reason"]) {
+                $(`#${detail["key"]}`).removeClass("active");
+            } else {
+                member_num[detail["rank"]]++;
             }
         }
+        update_ui();
     }
     read_from_local();
 
-    function add_member(name, rank, hash="") {
-        if (hash == "") {
-            hash = MD5.generate(name);
+
+    function add_member(name, rank, id="") {
+        if (!id) {
+            id = hash(name);
         }
         let box = $("<div></div>").attr({
             class: "col-auto",
-            id: `div-${hash}`,
+            id: `div-${id}`,
         });
-        let member = $(`<button>${name}</button>`).attr({
+        let cls = `btn ${BTN_CLASS[rank]}`;
+        if (!selected_reason) {
+            cls += " active";
+        }
+        let btn = $(`<button>${name}</button>`).attr({
             type: "button",
-            class: `btn ${BTN_CLASS[rank]}`,
-            id: hash,
+            class: cls,
+            id: id,
+            // "data-bs-toggle": "button",
+        });
+        if (selected_reason) {
+            btn.click(select_member_event);
+        }
+        box.append(btn);
+        $(`${RANKS[rank]}-list`).append(box);
+        member_rank[id] = rank;
+        return id;
+    }
+
+
+    function add_reason(name, id="") {
+        if (!id) {
+            id = hash(name);
+        }
+        let box = $("<div></div>").attr({
+            class: "col-auto",
+            id: `div-${id}`,
+        });
+        let btn = $(`<button>${name}</button>`).attr({
+            type: "button",
+            class: "btn btn-outline-primary",
+            id: id,
             "data-bs-toggle": "button",
         });
-        member.click(select_member_event);
-
-        box.append(member);
-        $(`${RANKS[rank]}-list`).append(box);
-        // $(RANKS[rank]+"label").removeClass("hidden");
-
-        member_rank[hash] = rank;
-        return hash;
+        btn.click(select_reason_event);
+        box.append(btn);
+        $("#reason-list").append(box);
+        return id;
     }
+
+    function delete_members() {
+        let remains = [];
+        members.forEach(member => {
+            if (!members_to_delete.has(member)) {
+                remains.push(member);
+            }
+        });
+        members = Array.from(remains);
+        localStorage.setItem("members", JSON.stringify(members));
+
+        members_to_delete.forEach(member => {
+            let detail = JSON.parse(localStorage.getItem(`m-${member}`));
+            if (!detail["reason"]) {
+                member_num[detail["rank"]]--;
+            }
+            $(`#div-${member}`).remove();
+            delete member_rank[member];
+            localStorage.removeItem(`m-${member}`);
+        });
+        members_to_delete.clear();
+        update_ui();
+    }
+
+
+
+    function delete_reasons() {
+        let remains = [];
+        reasons.forEach(reason => {
+            if (!reasons_to_delete.has(reason)) {
+                remains.push(reason);
+            }
+        });
+        reasons = Array.from(remains);
+        localStorage.setItem("reasons", JSON.stringify(reasons));
+
+        members.forEach(id => {
+            let detail = JSON.parse(localStorage.getItem(`m-${id}`));
+            if (detail["reason"] && reasons_to_delete.has(hash(detail["reason"]))) {
+                detail["reason"] = "";
+                localStorage.setItem(`m-${id}`, JSON.stringify(detail));
+                member_num[detail["rank"]]++;
+            }
+        });
+        reasons_to_delete.forEach(id => {
+            $(`#div-${id}`).remove();
+            localStorage.removeItem(`r-${id}`);
+        });
+        reasons_to_delete.clear();
+        update_ui();
+    }
+
+
+    function select_reason_event() {
+        if (delete_mode) {
+            reasons_to_delete.add($(this).attr("id"));
+        } else {
+            if (selected_reason) {
+                let reason = $(`#${hash(selected_reason)}`);
+                deactivate(reason, select_reason_event);
+            }
+            selected_reason = $(this).html();
+            for (const id of members) {
+                let detail = JSON.parse(localStorage.getItem(`m-${id}`));
+                let member = $(`#${id}`);
+                if (detail["reason"] === selected_reason) {
+                    activate(member, deselect_member_event);
+                } else if (detail["reason"] != selected_reason) {
+                    deactivate(member, select_member_event);
+                }
+            }
+        }
+        activate($(this), deselect_reason_event);
+    }
+
+
+    function deselect_reason_event() {
+        if (delete_mode) {
+            reasons_to_delete.delete($(this).attr("id"));
+        } else {
+            selected_reason = "";
+            show_presented_members();
+        }
+        deactivate($(this), select_reason_event);
+    }
+
+
+    function show_presented_members() {
+        for (const id of members) {
+            let member = $(`#${id}`);
+            member.off("click");
+            let detail = JSON.parse(localStorage.getItem(`m-${id}`));
+            if (detail["reason"]) {
+                member.removeClass("active");
+            } else {
+                member.addClass("active");
+            }
+        }
+    }
+
 
     function select_member_event() {
-        select_member($(this));
-        let id = $(this).attr("id");
-        let data = JSON.parse(localStorage.getItem(id));
-        data["selected"] = true;
-        localStorage.setItem(id, JSON.stringify(data));
+        let member = $(this);
+        activate(member, deselect_member_event)
+        
+        if (delete_mode) {
+            members_to_delete.add(member.attr("id"));
+        }
+        else {
+            let key = `m-${member.attr("id")}`;
+            let detail = JSON.parse(localStorage.getItem(key));
+            if (!detail["reason"]) {
+                let rank = member_rank[member.attr("id")];
+                member_num[rank]--;
+                update_ui(rank);
+            }
+            detail["reason"] = selected_reason;
+            localStorage.setItem(key, JSON.stringify(detail));
+        }
     }
-
-    function select_member(member) {
-        let rank = member_rank[member.attr("id")];
-        // $(this).removeClass(BTN_CLASS[rank]).addClass(BTN_SELECTED_CLASS[rank]);
-        member.addClass("active");
-        member.off("click", select_member_event);
-        member.click(deselect_member_event);
-
-        member_num[rank]++;
-        selected_members.add(member.attr("id"));
-        update_ui(rank);
-    }
-
     
+
     function deselect_member_event() {
-        deselect_member($(this));
-        let id = $(this).attr("id");
-        let data = JSON.parse(localStorage.getItem(id));
-        data["selected"] = false;
-        localStorage.setItem(id, JSON.stringify(data));
-    }
+        let member = $(this);
+        deactivate(member, select_member_event);
+        
+        if (delete_mode) {
+            members_to_delete.delete(member.attr("id"));
+        } else {
+            let rank = member_rank[member.attr("id")];
+            member_num[rank]++;
+            update_ui(rank);
 
-    function deselect_member(member) {
-        let rank = member_rank[member.attr("id")];
-        // $(this).removeClass(BTN_SELECTED_CLASS[rank]).addClass(BTN_CLASS[rank]);
-        member.removeClass("active");
-        member.off("click", deselect_member_event);
-        member.click(select_member_event);
-
-        member_num[rank]--;
-        selected_members.delete(member.attr("id"));
-        update_ui(rank);
+            let key = `m-${$(this).attr("id")}`;
+            let detail = JSON.parse(localStorage.getItem(key));
+            detail["reason"] = "";
+            localStorage.setItem(key, JSON.stringify(detail));
+        }
     }
 
 
@@ -116,46 +271,80 @@ $(function() {
         } else {
             $(`${RANKS[rank]}-num`).text(member_num[rank]);
         }
-        $("#total-num").text(selected_members.size);
+        $("#total-num").text(member_num.reduce((a,b) => a+b));
     }
 
-
-    function delete_members() {
-        selected_members.forEach(member => {
-            $(`#div-${member}`).remove();
-            delete member_rank[member];
-            localStorage.removeItem(member);
-        });
-        selected_members.clear();
-        for (let i = 0; i < member_num.length; i++) {
-            member_num[i] = 0;
-        }
-        update_ui();
-    }
 
     let options_opened = false;
-    $("#options-btn").click(function() {
+    function toggle_options() {
         if (options_opened) {
             $("#add-member-div").hide();
             $("#add-reason-div").hide();
-            $("#options-img").attr("src", "assets/options.png");
+            $("#options-btn").html("<i class=\"bi bi-plus-circle\"></i>");
+            $("#deletion-btn").css("opacity", "1");
+            $("#deletion-btn").click(toggle_deletion);
         } else {
             $("#add-member-div").show();
             $("#add-reason-div").show();
-            $("#options-img").attr("src", "assets/options-selected.png");
+            $("#options-btn").html("<i class=\"bi bi-plus-circle-fill\"></i>");
+            $("#deletion-btn").css("opacity", "0.5");
+            $("#deletion-btn").off("click");
         }
         options_opened = !options_opened;
+    }
+
+    $("#options-btn").click(toggle_options);
+
+
+    function toggle_deletion() {
+        if (delete_mode) {
+            $("#delete-div").hide();
+            $("#deletion-btn").html("<i class=\"bi bi-trash3\"></i>");
+            $("#options-btn").css("opacity", "1");
+            $("#options-btn").click(toggle_options);
+            reasons.forEach(reason => {
+                deactivate($(`#${reason}`), select_reason_event);
+            });
+            show_presented_members();
+        } else {
+            $("#delete-div").show();
+            $("#deletion-btn").html("<i class=\"bi bi-trash3-fill\"></i>");
+            $("#options-btn").css("opacity", "0.5");
+            $("#options-btn").off("click");
+
+            reasons.forEach(reason => {
+                deactivate($(`#${reason}`), select_reason_event);
+            });
+            members.forEach(member => {
+                deactivate($(`#${member}`), select_member_event);
+            });
+        }
+        delete_mode = !delete_mode;
+    }
+    $("#deletion-btn").click(toggle_deletion);
+
+    let info_opened = false;
+    $("#info-btn").click(function() {
+        if (info_opened) {
+            $("#quickstart").hide();
+            $("#info-btn").html("<i class=\"bi bi-info-circle\"></i>");
+        } else {
+            $("#quickstart").show();
+            $("#info-btn").html("<i class=\"bi bi-info-circle-fill\"></i>");
+        }
+        info_opened = !info_opened;
     });
+
 
     $("#add-member-btn").click(function() {
         let name = $("#input-member").val();
         if (!name) {
-            alert("請輸入人名");
+            alert("請輸入人員");
             return;
         }
-        let id = MD5.generate(name);
+        let id = hash(name);
         if (id in member_rank) {
-            alert("人名重複了");
+            alert("人員重複了");
             return;
         }
         let rank = parseInt($("#input-class option:selected").val());
@@ -163,26 +352,68 @@ $(function() {
             alert("請選擇階級");
             return;
         }
-        let hash = add_member(name, rank, id);        
-        localStorage.setItem(id, JSON.stringify({
+        add_member(name, rank, id);        
+        localStorage.setItem(`m-${id}`, JSON.stringify({
             "name": name,
             "rank": rank,
             "selected": false,
-            "key": hash,
-            "index": ++counts,
+            "key": id,
         }));
         $("#input-member").val('');
+
+        member_num[rank]++;
+        update_ui();
+        members.push(id);
+        localStorage.setItem("members", JSON.stringify(members));
     });
 
-    $("#delete-btn").click(delete_members);
+
+    $("#add-reason-btn").click(function() {
+        let reason = $("#input-reason").val();
+        $("#input-reason").val('');
+        if (!reason) {
+            alert("請輸入事故");
+            return;
+        }
+        let id = hash(reason);
+        if (`r-${id}` in localStorage) {
+            alert("事故重複了");
+            return;
+        }
+        add_reason(reason, id);
+        reasons.push(id);
+        localStorage.setItem(`r-${id}`, reason);
+        localStorage.setItem("reasons", JSON.stringify(reasons));
+    });
+
+
+    $("#delete-selection-btn").click(function() {
+        delete_members();
+        delete_reasons();
+        reasons.forEach(reason => {
+            deactivate($(`#${reason}`), select_reason_event);
+        });
+        members.forEach(member => {
+            deactivate($(`#${member}`), select_member_event);
+        });
+    });
+
 
     $("#delete-all-btn").click(function() {
-        delete_members();
-        for (const member in member_rank) {
+        for (const member of members) {
             $(`#div-${member}`).remove();
-            delete member_rank[member];
         }
+        members = [];
+        members_to_delete.clear();
+
+        for (const reason of reasons) {
+            $(`#div-${reason}`).remove();
+        }
+        reasons = [];
+        reasons_to_delete.clear();
+
+        member_num.fill(0);
+        update_ui();
         localStorage.clear();
-        counts = 0;
     })
 });
